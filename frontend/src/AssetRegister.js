076b8b4b4a6fd07a727b3f141fa0911b1f3fd694
INNOVATION LAB-10 <INNOVATION LAB-10>
2017-06-12 16:07:17 +0530

    frontend with asset register w/o UID generation v0.2

--- frontend/src/AssetRegister.js
+++ frontend/src/AssetRegister.js
@@ -8,11 +8,15 @@ import { Router, Route, Link, browserHistory, IndexRoute } from 'react-router';
 
  
  
-class AssetRegister extends Component {
+import React, { Component } from 'react';
+ 
+ 
+ 
+class AssetRegistration extends Component {
 constructor(){
     super();
     this.state ={
-        newAsset: {},
+        newProject: {},
         formSub : false,
         assetName: '',
         assetQuantity:'',
@@ -20,7 +24,10 @@ constructor(){
         totalPrice:''
     }
     this.updateState = this.updateState.bind(this);
+    this.updateState1 = this.updateState1.bind(this);
+    this.updateState2 = this.updateState2.bind(this);
     this.renderSubmit = this.renderSubmit.bind(this);
+    this.renderTotal = this.renderTotal.bind(this);
  
     }
  
@@ -30,11 +37,11 @@ constructor(){
         categories: ['Bonds','Nano Stocks','Penny Stocks']
     }
     handleSubmit(e){
-    //   if(this.refs.assetName.value === ''){
-    //       alert('input is required');
+    //   if(this.refs.title.value === ''){
+    //       alert('Amount is required');
     //   }else{
-    //       this.setState({newAsset:{
-    //           assetName: this.refs.assetName.value,
+    //       this.setState({newProject:{
+    //           title: this.refs.title.value,
     //           category: this.refs.category.value
     //       }, formSub : "true"},function(){
     //           console.log("hi",this.state);
@@ -46,23 +53,35 @@ constructor(){
     //     e.preventDefault();
        
     }
-    updateTotal(e){
-        this.setState({totalPrice: e.target.assetQuantity * e.target.pricePerAsset});
-    }
+  
     updateState(e) {
-      this.setState({assetName: e.target.value,
-                   
-                    pricePerAsset: e.target.value});
-   }
+      this.setState({assetName: e.target.value});
+    }
+ 
+    updateState2(e){                   
+        this.setState({pricePerAsset: e.target.value});
+}
    updateState1(e) {
-      this.setState({
-                    assetQuantity: e.target.value});
+      this.setState({assetQuantity: e.target.value});
+      /*let c = (this.state.assetQuantity * this.state.pricePerAsset);
+      console.log(c)
+      this.setState({totalPrice: c});*/
+      this.renderTotal(this.state.assetQuantity, this.state.pricePerAsset);
+     
    }
+ 
    renderSubmit() {
       console.log()
       if(this.state.formSub)
       {return (<div >You have assigned with {this.state.data} CT Tokens</div>);}
       else return false;
+   }
+   renderTotal () {
+       let c = (this.state.assetQuantity * this.state.pricePerAsset);
+       //console.log("here", assetQuantity, pricePerAsset);
+      console.log("total", c)
+      this.setState({totalPrice: c});
+      return (<div>{this.state.totalPrice}</div>)
    }
   
   render() {
@@ -75,18 +94,19 @@ constructor(){
       <form onSubmit={this.handleSubmit.bind(this)}>   
         <div>
         <label>Enter Asset name :</label>
-        <input type="text" id="input1" ref="title1" value = {this.state.assetName} onChange = {this.updateState}/><br />
-        <label>Enter Quantity of Asset :</label>
-        <input type="text" id="input2" ref="title2" value = {this.state.assetQuantity} onChange = {this.updateState1}/><br />
+        <input type="text" id="input1" ref="title1" value = {this.state.assetName} onChange = {this.updateState}/><br />   
         <label>Enter Price per Asset :</label>
-        <input type="text" id="input3" ref="title3" value = {this.state.pricePerAsset} onChange = {this.updateState}/><br />
+        <input type="text" id="input3" ref="title3" value = {this.state.pricePerAsset} onChange = {this.updateState2}/><br />
+        <label>Enter Quantity of Asset :</label>\
+        <input type="text" id="input2" ref="title2" value = {this.state.assetQuantity} onChange = {this.updateState1} onKeyUp={this.renderTotal} /><br />
+        {this.renderTotal}
         </div>
         <div>
         <label>Asset Type :</label>
         <select ref="category" >
         {categoryOptions}
         </select><br /><br />
-        </div>
+       </div>
         <div>
         <label> Total Market value of Assets :</label>
         <input type="text" id="input4" ref="title4" value = {this.state.totalPrice} /><br />
@@ -94,11 +114,11 @@ constructor(){
         <input type="submit" value="Submit"/>
         
       </form>
-        {this.renderSubmit()}
+        {this.renderSubmit}
  
          </div>
     );
   }
 }
  
-export default AssetRegister;
+export default AssetRegistration;
