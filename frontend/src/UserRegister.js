094138e7513ecdf5bb7587df242deb311f4f666a
sachdev18 <sdvr.18@gmail.com>
2017-06-13 13:46:06 +0530

    added css for user register

--- frontend/src/UserRegister.js
+++ frontend/src/UserRegister.js
@@ -25,24 +25,25 @@ class UserRegister extends Component {
     this.updateState = this.updateStateCP.bind(this);
   }
 
-  updateStateFN(e) {
-    this.setState({firstName: e.target.value});
+  updateStateALL(e) {
+    this.setState([e.target.name]: e.target.value});
   }
-  updateStateLN(e) {
-    this.setState({lastName: e.target.value});
-  }
-  updateStateMN(e) {
-    this.setState({mobileNumber: e.target.value});
-  }
-  updateStatePI(e) {
-    this.setState({panID: e.target.value});
-  }
-  updateStatePa(e) {
-    this.setState({password: e.target.value});
-  }
-  updateStateCP(e) {
-    this.setState({confirmPassword: e.target.value});
-  }
+  // updateStateALL(e) {
+  //   this.setState({lastName: e.target.value});
+  // }
+  // updateStateALL(e) {
+  //   this.setState({mobileNumber: e.target.value});
+  // }
+  // updateStateALL(e) {
+  //   this.setState({panID: e.target.value});
+  // }
+  // updateStateALL(e) {
+  //   this.setState({password: e.target.value});
+  // }
+  // updateStateALL(e) {
+  //   this.setState({confirmPassword: e.target.value});
+  // }
+
 
     render() {
         return (
@@ -54,24 +55,23 @@ class UserRegister extends Component {
 
                 <div id="user-register-input-box" className="Register-box">
 
-                    <label id="user-firstName" value = {this.state.firstName} onChange = {this.updateStateALL}>First Name</label><br />
-                    <input id="user-firstName-input">Please enter Firstname</input><br />
+                    <label id="user-firstName" >First Name</label><br />
+                    <input id="user-firstName-input" onChange = {this.updateStateALL} value = {this.state.firstName} name="firstName">Please enter Firstname</input><br />
 
-                    <label id="user-lastName" value = {this.state.assetName} onChange = {this.updateStateALL}>Last Name</label><br />
-                    <input id="user-lastName-input">Please enter Lastname</input><br />
+                    <label id="user-lastName">Last Name</label><br />
+                    <input id="user-lastName-input" value = {this.state.lastName} onChange = {this.updateStateALL}>Please enter Lastname</input><br />
 
-                    <label id="user-mobile" value = {this.state.assetName} onChange = {this.updateStateALL}>Mobile Number</label><br />
-                    <input id="user-mobile-input" type="number" minLength='10' >Please enter mobile number</input><br />
+                    <label id="user-mobile">Mobile Number</label><br />
+                    <input id="user-mobile-input" type="number" minLength='10' value = {this.state.mobileNumber} onChange = {this.updateStateALL}>Please enter mobile number</input><br />
 
-                    <label id="user-PanID" value = {this.state.assetName} onChange = {this.updateStateALL}>PAN ID</label><br />
-                    <input id="user-PanID-input">Please enter PanID</input><br />
+                    <label id="user-PanID" >PAN ID</label><br />
+                    <input id="user-PanID-input" value = {this.state.panID} onChange = {this.updateStateALL}>Please enter PanID</input><br />
 
-                    <label id="user-password" value = {this.state.assetName} onChange = {this.updateStateALL}>Password</label><br />
-                    <input id="user-password-input">Please enter password</input><br />
+                    <label id="user-password">Password</label><br />
+                    <input id="user-password-input" value = {this.state.password} onChange = {this.updateStateALL}>Please enter password</input><br />
 
-                    <label id="confirm-password" value = {this.state.assetName} onChange = {this.updateStateALL}>Confirm Password</label><br />
-                    <input id="confirm-password-input">Please confirm password</input><br />
-
+                    <label id="confirm-password">Confirm Password</label><br />
+                    <input id="confirm-password-input" value = {this.state.confirmPassword} onChange = {this.updateStateALL}>Please confirm password</input><br />
                     <button id="login-submit-button" type="button">Submit</button>
                     <button id="login-reset-button" type="button">Reset</button>
 
