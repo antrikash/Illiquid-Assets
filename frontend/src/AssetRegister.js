45cd851150ba28449294498a21c90648acfcf8e9
INNOVATION LAB-10 <INNOVATION LAB-10>
2017-06-13 14:36:16 +0530

    updated assetregister with UID generation

--- frontend/src/AssetRegister.js
+++ frontend/src/AssetRegister.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react';
 
-import './App.css';
-import Web3 from 'web3';
-import 'bootstrap/dist/css/bootstrap.css';
-import { Router, Route, Link, browserHistory, IndexRoute } from 'react-router';
 import hashFnv32a from './hash';
+// function hashFnv32a(str, asString, seed) {
+//     /*jshint bitwise:false */
+//     var i, l,
+//         hval = (seed === undefined) ? 0x811c9dc5 : seed;
+
+//     for (i = 0, l = str.length; i < l; i++) {
+//         hval ^= str.charCodeAt(i);
+//         hval += (hval << 1) + (hval << 4) + (hval << 7) + (hval << 8) + (hval << 24);
+//     }
+//     if( asString ){
+//         // Convert to 8 digit hex string
+//         return ("0000000" + (hval >>> 0).toString(16)).substr(-8);
+//     }
+//     return hval >>> 0;
+// }
 
 
 var aName;
-var assetUid;
+ var assetUid;
+
 class AssetRegistration extends Component {
+    
 constructor(){
     super();
     this.state ={
@@ -25,31 +38,35 @@ constructor(){
     this.updateState2 = this.updateState2.bind(this);
     this.renderSubmit = this.renderSubmit.bind(this);
     this.renderTotal = this.renderTotal.bind(this);
+    //this.generateUID = this.generateUID.bind(this);
 
     }
+
+
 
     static defaultProps ={
         categories: ['Bonds','Nano Stocks','Penny Stocks']
     }
     generateUID(e){
-       assetUid = hashFnv32a(aName,Date.now(),true);
-       alert("UID: "+assetUid.toString());
-       console.log(assetUid);
-        e.preventDefault();
-   }
+        assetUid = hashFnv32a(aName,Date.now(),true);
+        alert("UID: ",assetUid.toString());
+        console.log(assetUid);
+         e.preventDefault();
+
+    }
+   
     updateState(e) {
       this.setState({assetName: e.target.value});
+      aName = this.state.assetName;
     }
-    updateState2(e){
+
+    updateState2(e){                    
         this.setState({pricePerAsset: e.target.value});
 }
-    updateState1(e){
+   updateState1(e) {
       this.setState({assetQuantity: e.target.value});
-      /*let c = (this.state.assetQuantity * this.state.pricePerAsset);
-      console.log(c)
-      this.setState({totalPrice: c});*/
       this.renderTotal(this.state.assetQuantity, this.state.pricePerAsset);
-
+      
    }
 
    renderSubmit() {
@@ -59,42 +76,27 @@ constructor(){
       else return false;
    }
    renderTotal () {
-       let c = (this.state.assetQuantity * this.state.pricePerAsset);
-       //console.log("here", assetQuantity, pricePerAsset);
+      let c = (this.state.assetQuantity * this.state.pricePerAsset);
       console.log("total", c)
       this.setState({totalPrice: c});
       return (<div>{this.state.totalPrice}</div>)
    }
-//RENDER FUNCTION
+   
   render() {
       let categoryOptions = this.props.categories.map(category => {
           return <option key ={category} value={category}>{category}</option>
       });
      return(
       <div>
-      <div className="row">
-          <div id="header">
-              <span id="Heading-name">Home ></span>
-          </div>
-
       <h3><strong> Register yous assets </strong> </h3>
-      <form onSubmit={this.handleSubmit.bind(this)}>
-
-        <div id="user-Register-box" className="col-md-5">
-        Please Register here
-        </div>
-
-        <div id="asset-register-input-box" className="Register-box">
-
-        <label id="asset-Name">Enter Asset name :</label>
-        <input id="asset-firstName-input" type="text" ref="title1" value = {this.state.assetName} onChange = {this.updateState}/><br />
-
-        <label id="asset-lastName">Enter Price per Asset :</label>
-        <input id="asset-lastName-input" type="text" ref="title3" value = {this.state.pricePerAsset} onChange = {this.updateState2}/><br />
-
-        <label id="asset-mobile">Enter Quantity of Asset :</label>
-        <input id="asset-mobile-input" type="text" ref="title2" value = {this.state.assetQuantity} onChange = {this.updateState1} onKeyUp={this.renderTotal} /><br />
-
+      <form onSubmit={this.generateUID.bind(this)}>    
+        <div>
+        <label>Enter Asset name :</label>
+        <input type="text" id="input1" ref="title1" value = {this.state.assetName} onChange = {this.updateState}/><br />    
+        <label>Enter Price per Asset :</label>
+        <input type="text" id="input3" ref="title3" value = {this.state.pricePerAsset} onChange = {this.updateState2}/><br />
+        <label>Enter Quantity of Asset :</label>\
+        <input type="text" id="input2" ref="title2" value = {this.state.assetQuantity} onChange = {this.updateState1} onKeyUp={this.renderTotal} /><br />
         {this.renderTotal}
         </div>
         <div>
@@ -102,25 +104,19 @@ constructor(){
         <select ref="category" >
         {categoryOptions}
         </select><br /><br />
-       </div>
+        </div>
         <div>
         <label> Total Market value of Assets :</label>
         <input type="text" id="input4" ref="title4" value = {this.state.totalPrice} /><br />
         </div>
-        <input type="submit" value="Submit"/>
-
         <input type="submit" value="Submit" onClick = {this.generateUID}/>
-
+         
       </form>
         {this.renderSubmit}
-
-
-            <div id="footer" className="noPadding"></div>
-        </div>
 
          </div>
     );
   }
-}
+} 
 
 export default AssetRegistration;
